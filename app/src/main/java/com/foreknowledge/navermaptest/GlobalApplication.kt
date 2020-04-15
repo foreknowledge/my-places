28996c55456e7dbb6035b45a614d17cb9d650f30
s2choco <kyj_0129@naver.com>
2020-04-15 21:15:13 +0900

    [build] API 통신을 위한 gradle & manifest 설정

--- app/src/main/java/com/foreknowledge/navermaptest/GlobalApplication.kt
+++ app/src/main/java/com/foreknowledge/navermaptest/GlobalApplication.kt
@@ -2,23 +2,27 @@ package com.foreknowledge.navermaptest
 
 import android.app.Application
 import android.content.Context
+import com.naver.maps.map.NaverMapSdk
+import com.naver.maps.map.NaverMapSdk.NaverCloudPlatformClient
 
 /**
  * Create by Yeji on 11,April,2020.
  */
 class GlobalApplication: Application() {
     companion object {
-
-        private var APPLICATION_CONTEXT: Context? = null
+        private lateinit var APPLICATION_CONTEXT: Context
 
         @JvmStatic
         fun getContext(): Context {
-            return APPLICATION_CONTEXT!!
+            return APPLICATION_CONTEXT
         }
     }
 
     override fun onCreate() {
         super.onCreate()
         APPLICATION_CONTEXT = applicationContext
+
+        NaverMapSdk.getInstance(this).client =
+            NaverCloudPlatformClient(BuildConfig.NAVER_CLIENT_ID)
     }
 }
