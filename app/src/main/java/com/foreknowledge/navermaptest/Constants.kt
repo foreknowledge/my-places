f027f0b0a5b9e7253a3b3b73fd19b2829bdac39e
s2choco <kyj_0129@naver.com>
2020-04-16 18:17:53 +0900

    [feat] 현재 위치 overlay 추가 #2

--- app/src/main/java/com/foreknowledge/navermaptest/Constants.kt
+++ app/src/main/java/com/foreknowledge/navermaptest/Constants.kt
@@ -6,3 +6,4 @@ package com.foreknowledge.navermaptest
 const val BASE_URL = "https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/"
 const val HEADER_CLIENT_ID = "X-NCP-APIGW-API-KEY-ID"
 const val HEADER_CLIENT_SECRET = "X-NCP-APIGW-API-KEY"
+const val LOCATION_PERMISSION_REQUEST_CODE = 1000
