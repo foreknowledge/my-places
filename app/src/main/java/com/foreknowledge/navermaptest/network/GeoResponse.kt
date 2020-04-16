e41bdb1a46b758bd7a660b94545ed37036234fa1
s2choco <kyj_0129@naver.com>
2020-04-16 15:04:07 +0900

    [feat] API 요청 query 변경

--- app/src/main/java/com/foreknowledge/navermaptest/network/GeoResponse.kt
+++ app/src/main/java/com/foreknowledge/navermaptest/network/GeoResponse.kt
@@ -9,17 +9,28 @@ data class GeoResponse (
 
 data class ResultResponse (
 	val name: String,
-	val region: RegionResponse
+	val region: RegionResponse,
+	val land: LandResponse
 )
 
 data class RegionResponse (
 	val area0: AreaResponse,
 	val area1: AreaResponse,
 	val area2: AreaResponse,
-	val area3: AreaResponse,
-	val area4: AreaResponse
+	val area3: AreaResponse
 )
 
 data class AreaResponse (
 	val name: String
+)
+
+data class LandResponse (
+	val addition0: AdditionResponse,
+	val addition1: AdditionResponse,
+	val addition2: AdditionResponse
+)
+
+data class AdditionResponse (
+	val type: String,
+	val value: String
 )
--- app/src/main/java/com/foreknowledge/navermaptest/network/GeoService.kt
+++ app/src/main/java/com/foreknowledge/navermaptest/network/GeoService.kt
@@ -19,7 +19,10 @@ interface GeoService {
 	@GET("gc")
 	fun getAddress(
 		@Query("coord") coord: String,
+		// 좌표 체계: UTM-K
 		@Query("sourcecrs") sourcecrs: String = "nhn:2048",
+		// 변환 작업: 좌표 -> 법정동 & 도로명 주소
+		@Query("orders") orders: String = "legalcode,roadaddr",
 		@Query("output") output: String = "json"
 	): Call<GeoResponse>
 }
